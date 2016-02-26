26271a746cb489d53497c977eb87586e5a5bc986
andre.rogerio <andre.rogerio@nexxera.com>
2016-02-26 09:34:08 -0300

    Exemplo python

---
+++ src/game/ball.py
@@ -0,0 +1,13 @@
+from src.game.game_object import GameObject
+from src.hello_pong import canvas
+
+
+class Ball(GameObject):
+    def __init__(self, canvas, x, y):
+        self.radius = 10
+        self.direction = [1, -1]
+        self.speed = 10
+        item = canvas.create_oval(x-self.radius, y-self.radius,
+                                  x+self.radius, y+self.radius,
+                                  fill='white')
+        super(Ball, self).__init__(canvas, item)
